8c6e24c54481351410e5888be6e65c71be90fe3d
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-06-20 09:59:05 +0530

    dashboard

---
+++ lib/Dashboard.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatelessWidget {
+  const Dashboard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Dash(),
+    );
+  }
+}
+
+class Dash extends StatefulWidget {
+  const Dash({Key? key}) : super(key: key);
+
+  @override
+  State<Dash> createState() => _DashState();
+}
+
+class _DashState extends State<Dash> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(30.0),
+            bottomRight: Radius.circular(30.0),
+          ),
+        ),
+        leading: null,
+        actions: <Widget>[],
+        title: Text(
+          '                 Tutorial',
+          style: TextStyle(
+              fontSize: 30.0, fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+        backgroundColor: Color(0xff237ACC),
+      ),
+      body: Column(
+        children: [
+          Spacer(),
+          Row(
+            children: [
+              Spacer(),
+              Text(
+                'Tutorial',
+                style: TextStyle(fontSize: 30.0),
+              ),
+              Spacer(),
+            ],
+          ),
+          Spacer(),
+        ],
+      ),
+    );
+  }
+}
